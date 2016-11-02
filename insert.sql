f1286cd3bfd8048516b1625e0345cbe774cfc3f8
Alexandre <alexdu98@users.noreply.github.com>
2016-11-02 18:46:20 +0100

    Ajout pokestop

--- insert.sql
+++ insert.sql
@@ -1,6 +1,6 @@
 /* Deletion des rows pour toutes les tables */
 
-DELETE FROM Dresseur ;
+DELETE FROM Dresseur;
 DELETE FROM Pokemon;
 DELETE FROM Niveau;
 DELETE FROM Item;
@@ -22,39 +22,39 @@ UPDATE Pokemon SET evolution = 'Tortank' WHERE race = 'Carabaffe';
 
 INSERT INTO Pokemon VALUES ('Insecateur', 0, 'Vol', NULL, NULL);
 
-INSERT INTO Pokemon VALUES ('Bulbizarre', 25,'Plante',NULL,NULL);
-INSERT INTO Pokemon VALUES ('Herbizarre', 50,'Plante',NULL,'Bulbizarre');
-INSERT INTO Pokemon VALUES ('Florizarre', 0,'Plante',NULL,'Herbizarre');
+INSERT INTO Pokemon VALUES ('Bulbizarre', 25, 'Plante', NULL, NULL);
+INSERT INTO Pokemon VALUES ('Herbizarre', 50, 'Plante', NULL, 'Bulbizarre');
+INSERT INTO Pokemon VALUES ('Florizarre', 0, 'Plante',NULL, 'Herbizarre');
 UPDATE Pokemon SET evolution = 'Herbizarre' WHERE race = 'Bulbizarre';
 UPDATE Pokemon SET evolution = 'Florizarre' WHERE race = 'Herbizarre';
 
-INSERT INTO Pokemon VALUES ('Salameche', 25, 'Feu', NULL,NULL);
-INSERT INTO Pokemon VALUES ('Reptincel', 50, 'Feu',NULL,'Salameche');
-INSERT INTO Pokemon VALUES ('Dracaufeu', 0, 'Feu',NULL, 'Reptincel');
+INSERT INTO Pokemon VALUES ('Salameche', 25, 'Feu', NULL, NULL);
+INSERT INTO Pokemon VALUES ('Reptincel', 50, 'Feu', NULL, 'Salameche');
+INSERT INTO Pokemon VALUES ('Dracaufeu', 0, 'Feu', NULL, 'Reptincel');
 UPDATE Pokemon SET evolution = 'Reptincel' WHERE race = 'Salameche';
 UPDATE Pokemon SET evolution = 'Dracaufeu' WHERE race = 'Reptincel';
 
 Prompt Insertion des niveaux;
-INSERT INTO Niveau VALUES (1,0,1000);
-INSERT INTO Niveau VALUES (2,1000,3000);
-INSERT INTO Niveau VALUES (3,3000,6000);
-INSERT INTO Niveau VALUES (4,6000,10000);
-INSERT INTO Niveau VALUES (5,10000,15000);
-INSERT INTO Niveau VALUES (6,15000,21000);
-INSERT INTO Niveau VALUES (7,21000,28000);
-INSERT INTO Niveau VALUES (8,28000,36000);
-INSERT INTO Niveau VALUES (9,36000,45000);
-INSERT INTO Niveau VALUES (10,45000,55000);
+INSERT INTO Niveau VALUES (1, 0, 1000);
+INSERT INTO Niveau VALUES (2, 1000, 3000);
+INSERT INTO Niveau VALUES (3, 3000, 6000);
+INSERT INTO Niveau VALUES (4, 6000, 10000);
+INSERT INTO Niveau VALUES (5, 10000, 15000);
+INSERT INTO Niveau VALUES (6, 15000, 21000);
+INSERT INTO Niveau VALUES (7, 21000, 28000);
+INSERT INTO Niveau VALUES (8, 28000, 36000);
+INSERT INTO Niveau VALUES (9, 36000, 45000);
+INSERT INTO Niveau VALUES (10, 45000, 55000);
 
 Prompt Insertion des items;
 
-INSERT INTO Item VALUES ('Pokeball','Capture les pokemons sauvages. Se jette comme une balle.');
-INSERT INTO Item VALUES ('Encens','Attire les pokemon sauvages vers vous pendant 30 min.');
-INSERT INTO Item VALUES ('Leurre','Attire les pokemons sur un pokestop pour tous les joueurs.');
-INSERT INTO Item VALUES ('Potion','Rend 20 points de vie.');
-INSERT INTO Item VALUES ('Super potion','Rend 50 points de vie.');
-INSERT INTO Item VALUES ('Hyper potion','Rend 200 points de vie.');
-INSERT INTO Item VALUES ('Baie','Rend le pokemon plus apte a etre capture.');
+INSERT INTO Item VALUES ('Pokeball', 'Capture les pokemons sauvages. Se jette comme une balle.');
+INSERT INTO Item VALUES ('Encens', 'Attire les pokemon sauvages vers vous pendant 30 min.');
+INSERT INTO Item VALUES ('Leurre', 'Attire les pokemons sur un pokestop pour tous les joueurs.');
+INSERT INTO Item VALUES ('Potion', 'Rend 20 points de vie.');
+INSERT INTO Item VALUES ('Super potion', 'Rend 50 points de vie.');
+INSERT INTO Item VALUES ('Hyper potion', 'Rend 200 points de vie.');
+INSERT INTO Item VALUES ('Baie', 'Rend le pokemon plus apte a etre capture.');
 
 PROMPT Insertion des succes
 INSERT INTO Succes VALUES (1, 'Joggeur', 'Parcourez X km');
@@ -75,8 +75,11 @@ INSERT INTO Succes VALUES (7, 'Combattante', 'Remportez X combats en arene');
 
 Prompt Insertion de pokestop;
 
-INSERT INTO pokestop VALUES(1, coordonnees_t(48.873729, 2.294920),'Arc de triomphe');
-INSERT INTO pokestop VALUES(2, coordonnees_t(48.865487, 2.321141),'Place de la concorde');
+INSERT INTO pokestop VALUES(1, coordonnees_t(48.873729, 2.294920), 'Arc de triomphe');
+INSERT INTO pokestop VALUES(2, coordonnees_t(48.865487, 2.321141), 'Place de la concorde');
+INSERT INTO pokestop VALUES(3, coordonnees_t(43.631789, 3.866898), 'Faculte des Sciences de Montpellier');
+INSERT INTO pokestop VALUES(4, coordonnees_t(43.618999, 3.869439), 'Stade Philipides de Montpellier');
+INSERT INTO pokestop VALUES(5, coordonnees_t(46.72658, 2.485807), 'Gare d''Orval');
 
 PROMPT Insertion de Dresseur;
 
