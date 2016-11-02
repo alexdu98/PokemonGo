d5f79b21c232fc80414469f169d1b55eab934e5d
Alexandre <alexdu98@users.noreply.github.com>
2016-11-02 18:29:04 +0100

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -81,26 +81,52 @@ INSERT INTO pokestop VALUES(2, coordonnees_t(48.865487, 2.321141),'Place de la c
 PROMPT Insertion de Dresseur;
 
 /* Ajout du dresseur Pierre, de ses oeufs et de ses Pokemon captures */
-INSERT INTO Dresseur VALUES (0, 
+INSERT INTO Dresseur VALUES (1, 
 			Avatar_t('M', 'Brun', 'Bronzee', 'Marron'), 
 			'Pierre le pecheur', 
 			Coordonnees_t(10.3, 89.5), 
-			1200, 2, '15-JUL-2016', NULL, 120, 62, 
+			1200, 2, '01-JUL-2016', NULL, 120, 62, 
 			NULL, 
 			Liste_Oeufs(Oeuf_t(1, 0, 2, 5), Oeuf_t(2, 0, 3, 5))
 );
 
-INSERT INTO Dresseur VALUES (1, 
+INSERT INTO Dresseur VALUES (2, 
 			Avatar_t('F', 'Rousse', 'Blanche', 'Vert'), 
 			'Ondine a bicyclette', 
 			Coordonnees_t(26.8, 42.5), 
-			11000, 5, '02-JUL-2016', Equipe_t('rouge'), 120, 62, 
+			11000, 5, '04-JUL-2016', Equipe_t('rouge'), 120, 62, 
 			NULL, 
 			Liste_Oeufs(Oeuf_t(3, 1, 7, 10), Oeuf_t(4, 1, 1, 2), Oeuf_t(5, 1, 8, 10), Oeuf_t(6, 0, 3, 5))
 );
 
-Prompt Insertion de pokemon sauvage;
+INSERT INTO Dresseur VALUES(3,
+			Avatar_t('M', 'Brun', 'Blanche', 'Marron'), 
+			'Sacha', 
+			Coordonnees_t(43.60357, 3.91561),
+			12000, 5, '15-JUL-2016', Equipe_t('bleu'), 100, 10,
+			NULL,
+			Liste_Oeufs(Oeuf_t(7, 1, 8, 10), Oeuf_t(8, 0, 0, 2))
+);
 
+INSERT INTO Dresseur VALUES(4,
+			Avatar_t('M', 'Blond', 'Noir', 'Marron'), 
+			'Zaakary', 
+			Coordonnees_t(48.860611, 2.337644),
+			37000, 9, '15-JUL-2016', Equipe_t('jaune'), 0, 0,
+			NULL,
+			NULL
+);
+
+INSERT INTO Dresseur VALUES(5,
+			Avatar_t('F', 'Brunne', 'Blanche', 'Bleu'), 
+			'Agatha', 
+			Coordonnees_t(46.435453, 3.054045),
+			20000, 6, '29-JUL-2016', Equipe_t('rouge'), 52, 0,
+			NULL,
+			Liste_Oeufs(Oeuf_t(9, 1, 3, 5))
+);
+
+Prompt Insertion de pokemon sauvage;
 INSERT INTO Pokemon_Sauvage VALUES(1, 'Pikachu', Coordonnees_t(48.86361, 2.31198451));
 INSERT INTO Pokemon_Sauvage VALUES(2, 'Raichu', Coordonnees_t(48.984165, 2.984165));
 INSERT INTO Pokemon_Sauvage VALUES(3, 'Carapuce', Coordonnees_t(48.62512981, 2.269846));
@@ -109,3 +135,10 @@ INSERT INTO Pokemon_Sauvage VALUES(5, 'Tortank', Coordonnees_t(48.86361, 2.31198
 INSERT INTO Pokemon_Sauvage VALUES(6, 'Salameche', Coordonnees_t(43.6042, 3.8836));
 INSERT INTO Pokemon_Sauvage VALUES(7, 'Florizarre', Coordonnees_t(43.615225, 3.8462453));
 INSERT INTO Pokemon_Sauvage VALUES(8, 'Insecateur', Coordonnees_t(43.628962, 3.8660557));
+
+Prompt Insertion d arenes;
+INSERT INTO Arene VALUES(1, Coordonnees_t(43.635765, 3.847425), 'Occitania', Equipe_t('bleu'), 210, Liste_Defenseurs());
+INSERT INTO Arene VALUES(2, Coordonnees_t(43.603578, 3.915617), 'Odyseumia', Equipe_t('rouge'), 256, Liste_Defenseurs());
+INSERT INTO Arene VALUES(3, Coordonnees_t(43.608401, 3.879314), 'Comedia', Equipe_t(''), 451, Liste_Defenseurs());
+INSERT INTO Arene VALUES(4, Coordonnees_t(48.858214, 2.292516), 'TourEiffelia', Equipe_t('jaune'), 545, Liste_Defenseurs());
+INSERT INTO Arene VALUES(5, Coordonnees_t(48.860611, 2.337644), 'MuseeLouvria', Equipe_t('rouge'), 682, Liste_Defenseurs());
